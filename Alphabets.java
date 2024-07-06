97eeb0d8fbd5b89857d3ff7a3db4b6b5b74d538e
Vaishnavideokar22 <vaishnavideokar33@gmail.com>
2024-07-06 13:03:32 +0530

    Add files via upload

---
+++ Alphabets.java
@@ -0,0 +1,12 @@
+public class Alphabets {
+ public static void main(String []args){
+    
+            char ch = 'a';
+            while(ch <= 'z'){
+                System.out.print(ch + " ");
+                ch++;
+            }
+        }
+    }
+    
+
